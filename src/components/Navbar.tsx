b85c40e29f15c4d0a356f06e40f20ac8dbe74582
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-26 22:06:17 -0500

    created initial navbar skeleton

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 
 const Navbar = () => {
   return (
-    <div>
-        
-    </div>
-  )
+		<div className="flex justify-between p-5">
+			<div className="flex justify-center flex-row-reverse">
+				<h1>Waitress</h1>
+				<img className="w-16 h-16 rounded-full border border-gray-300 p-1 shadow-lg" src="https://cdn-icons-png.flaticon.com/512/9570/9570098.png" alt="Generic Waitress PNG" />
+			</div>
+			<div className="flex justify-center flex-row gap-4">
+				<Link to="/">Home</Link>
+				<Link to="/create-account">Create Account</Link>
+				<Link to="/login">Login</Link>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar
