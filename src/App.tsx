4b642754b0d0c5572cb4b741695d2ef99d09419a
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-11-27 22:56:59 -0500

    adjusted background image

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ import { Routes, Route } from 'react-router-dom'
 function App() {
 
   return (
-		<>
+		<div className="bg-[#E1D9D1]">
 			<Navbar />
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/create-account" element={<CreateAccount />} />
 				<Route path="/create-account" element={<Login />} />
 			</Routes>
-		</>
+		</div>
 	);
 }
 
