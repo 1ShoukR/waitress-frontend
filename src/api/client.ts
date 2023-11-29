ecf4d9a57c3785bebf6883ca34a95de8266f67bc
Rahmin <rahminshoukoohi@gmail.com>
2023-11-29 14:40:05 -0500

    added local host variables

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 export const client = axios.create({
-    baseURL: 'https://jsonplaceholder.typicode.com/',
-    // baseURL: "http://localhost:3000/api",
-    headers: {
-        "Content-Type": "application/json",
-    },
-})
+	// baseURL: 'https://jsonplaceholder.typicode.com/',
+	baseURL: 'http://127.0.0.1:3000/api',
+	headers: {
+		'Content-Type': 'application/json',
+	},
+});
 
 
