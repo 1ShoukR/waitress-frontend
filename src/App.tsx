dc749a80ad579d6f29c77003019a0415ad28dc99
Rahmin Shoukoohi <rahminshoukoohi@gmail.com>
2023-12-03 19:16:44 -0500

    adjusted route for login page

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/create-account" element={<CreateAccount />} />
-				<Route path="/create-account" element={<Login />} />
+				<Route path="/login" element={<Login />} />
 			</Routes>
 		</div>
 	);
