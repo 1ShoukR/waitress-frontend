10bb54696ecc23af574b8ff97973b8a1e322587b
1ShoukR <rahminshoukoohi@gmail.com>
2024-11-22 21:55:01 -0500

    refactor: updated colors and icon image

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Icon from '../assets/icon.png'
+import Icon from '../assets/icon.svg'
 
 
 
 const Navbar = () => {
   return (
-		<div className="flex justify-between items-center py-4 bg-[#f7f7f7] px-5">
+		<div className="flex justify-between items-center py-4 bg-[#F0EAD6] px-5">
 			<div className="flex items-center gap-4">
 				<Link to="/">
-					<img className="w-16 h-16 border  shadow-lg" src={Icon} alt="Generic Waitress PNG" />
+					<img className="w-20 h-20" src={Icon} alt="Generic Waitress PNG" />
 				</Link>
 				<Link to="/">
 					<h1 className="text-xl font-semibold ">Waitress</h1>
 				</Link>
 			</div>
 			<div className="flex gap-4">
-				<Link className="text-gray-700 hover:text-gray-900 font-medium" to="/create-account">
+				<Link className="text-black-700 hover:text-gray-900 font-medium" to="/create-account">
 					Create Account
 				</Link>
-				<Link className="text-gray-700 hover:text-gray-900 font-medium" to="/login">
+				<Link className="text-black-700 hover:text-gray-900 font-medium" to="/login">
 					Login
 				</Link>
 			</div>
