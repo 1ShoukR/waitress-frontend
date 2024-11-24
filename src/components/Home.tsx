1141aa2da25705cc8c41050f1955ad8c5c0a60b2
1ShoukR <rahminshoukoohi@gmail.com>
2024-11-24 00:35:02 -0500

    feat: added carousal of images to main homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,13 +1,40 @@
-import React from 'react'
-
-
+import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // Important!
 
 const Home = () => {
   return (
-		<div className="home-container bg-cover bg-center w-full flex justify-center items-center flex-col font-bold text-gray-700 text-2xl gap-4 p-6">
-			
-		</div>
-	);
-}
+    <div className="w-full max-w-6xl mx-auto">
+      <Carousel
+        showThumbs={false}
+        infiniteLoop={true}
+        autoPlay={true}
+        interval={3}
+      >
+        <div className="h-[500px]">
+          <img 
+            src="https://img.freepik.com/free-photo/restaurant-interior_1127-3394.jpg" 
+            alt="Restaurant Interior 1"
+            className="object-cover h-full w-full"
+          />
+        </div>
+        <div className="h-[500px]">
+          <img 
+            src="https://t4.ftcdn.net/jpg/02/94/26/33/360_F_294263329_1IgvqNgDbhmQNgDxkhlW433uOFuIDar4.jpg" 
+            alt="Restaurant Interior 2"
+            className="object-cover h-full w-full"
+          />
+        </div>
+        <div className="h-[500px]">
+          <img 
+            src="https://wallpapers.com/images/featured/restaurant-background-2ez77umko2vj5w02.jpg" 
+            alt="Restaurant Interior 3"
+            className="object-cover h-full w-full"
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
