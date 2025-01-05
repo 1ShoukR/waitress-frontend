3284bee0fc378a79e39278de267d517cfd33b400
1ShoukR <rahminshoukoohi@gmail.com>
2025-01-05 03:15:50 -0500

    added redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import Login from './components/Login'
 import CreateAccount from './components/CreateAccount'
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
-
   return (
-		<div className="">
-			<Navbar />
-			<Routes>
-				<Route path="/" element={<Home />} />
-				<Route path="/create-account" element={<CreateAccount />} />
-				<Route path="/login" element={<Login />} />
-			</Routes>
-		</div>
-	);
+    <Provider store={store}>
+      <div className="">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create-account" element={<CreateAccount />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Provider>
+  );
 }
 
 export default App
