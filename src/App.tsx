615397da7aa050569d68de92c787fd11567e9e77
1ShoukR <rahminshoukoohi@gmail.com>
2025-02-02 03:09:44 -0500

    updated routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store'
+import ProfileHomepage from './components/admin/Profile'
+import FloorplanHomepage from './components/admin/FloorplanHomepage'
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create-account" element={<CreateAccount />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<ProfileHomepage />} />
+          <Route path="/admin/floorplans" element={<FloorplanHomepage />} />
         </Routes>
       </div>
     </Provider>
