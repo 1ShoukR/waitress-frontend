c7ef97cef3d6381d5fff8222c83dea5ff5484488
1ShoukR <rahminshoukoohi@gmail.com>
2025-02-02 02:18:40 -0500

    refactor: Created setUser reducer

--- src/redux/authReducer.ts
+++ src/redux/authReducer.ts
@@ -1,13 +1,10 @@
-/**
- * Auth slice
- */
-import { createSlice } from '@reduxjs/toolkit';
-// import { doLogin } from './thunks';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { LoginResponse } from '../../types/types';
 
 interface AuthState {
     apiToken: string | null;
     authgroup: string | null;
-    userId: string | null;
+    userId: number | null;
     userName: string | null;
     userType: string | null;
     status: 'idle' | 'loading' | 'succeeded' | 'failed';
@@ -15,27 +12,35 @@ interface AuthState {
 }
 
 const initialState: AuthState = {
-	apiToken: null,
-	authgroup: null,
-	userId: null,
-	userName: null,
-	userType: null,
-	status: 'idle',
-	error: null,
+    apiToken: null,
+    authgroup: null,
+    userId: null,
+    userName: null,
+    userType: null,
+    status: 'idle',
+    error: null,
 };
-
 
 const authSlice = createSlice({
     name: 'auth',
     initialState,
     reducers: {
+        setUser: (state, action: PayloadAction<LoginResponse>) => { // Replace 'any' with your user type
+            state.status = 'succeeded';
+            state.apiToken = action.payload.token;
+            state.userId = action.payload.user.userId;
+            state.userName = action.payload.user.firstName + " " + action.payload.user.lastName;
+            state.userType = action.payload.user.authType;
+            state.error = null;
+        },
         loggedOut: (state) => {
             state.status = 'idle'
             state.apiToken = null
             state.userId = null;
-			state.userName = null;
+            state.userName = null;
         }
     }
-})
+});
 
+export const { setUser, loggedOut } = authSlice.actions;
 export default authSlice.reducer;
