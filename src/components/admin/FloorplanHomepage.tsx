0e24f75bb50617d5b0635bc4cb2c1895556d24d5
1ShoukR <rahminshoukoohi@gmail.com>
2025-03-31 00:42:51 -0400

    Update FloorplanHomepage.tsx

--- src/components/admin/FloorplanHomepage.tsx
+++ src/components/admin/FloorplanHomepage.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { useAppSelector } from '../../redux/hooks'
 
 
 const FloorplanHomepage = () => {
+  const userObject = useAppSelector((state) => state.auth)
+  console.log('user ob', userObject)
   return (
     <div>
       <p>Floorplan Homepage</p>
