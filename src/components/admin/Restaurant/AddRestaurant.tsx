404477a035f6cac67fc6164ef8b7b029fdc1a37f
1ShoukR <rahminshoukoohi@gmail.com>
2025-05-04 02:43:50 -0400

    created basic add restaurant component

---
+++ src/components/admin/Restaurant/AddRestaurant.tsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+
+
+const AddRestaurant = () => {
+  return (
+    <div>
+      <h1>Add Restaurant</h1>
+    </div>
+  );
+}
+
+export default AddRestaurant;
